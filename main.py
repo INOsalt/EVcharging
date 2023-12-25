43021b8922194781d6489e1e7ce8e544fecdfc95
INOsalt <wujunyuan263@gmail.com>
2023-12-25 17:17:33 +0800

    ver2

--- main.py
+++ main.py
@@ -6,25 +6,33 @@ import matplotlib.pyplot as plt
 # EVChargingDataGenerator类: 生成EV充电数据
 class EVChargingDataGenerator:
     def __init__(self):
-        # EV（电动汽车）正态分布参数
-        self.MU_1TC = 18; self.SIGMA_1TC = 3.3   # 家庭充电模式开始时间的平均值和标准差
-        self.MU_1TDIS = 8; self.SIGMA_1TDIS = 3.24  # 家庭充电模式结束时间的平均值和标准差
-        self.MU_2TC = 8.5; self.SIGMA_2TC = 3.3  # 公共充电模式开始时间的平均值和标准差
-        self.MU_2TDIS = 17.5; self.SIGMA_2TDIS = 3.24  # 公共充电模式结束时间的平均值和标准差
-
+        # 正态分布和电池相关参数
+        # 家庭充电模式参数
+        self.home_params = {
+            'MU_TC': 18, 'SIGMA_TC': 3.3,
+            'MU_TDIS': 8, 'SIGMA_TDIS': 3.24
+        }
+        # 公共充电模式参数
+        self.public_params = {
+            'MU_TC': 8.5, 'SIGMA_TC': 3.3,
+            'MU_TDIS': 17.5, 'SIGMA_TDIS': 3.24
+        }
         # 时间和电池相关参数
-        self.DELTA_T = 0.25  # 时间间隔（四分之一个小时）
-        self.SOC_CON_A = 0.1; self.SOC_CON_B = 0.3  # EV初始电池状态(SOC)的最小和最大值
-        self.SOC_MIN_A = 0.4; self.SOC_MIN_B = 0.6  # EV最小充电需求的最小和最大值
-        self.SOC_MAX_A = 0.8; self.SOC_MAX_B = 1.0  # EV最大充电容量的最小和最大值
+        self.DELTA_T = 0.25
+        self.SOC_CON_A = 0.1; self.SOC_CON_B = 0.3
+        self.SOC_MIN_A = 0.4; self.SOC_MIN_B = 0.6
+        self.SOC_MAX_A = 0.8; self.SOC_MAX_B = 1.0
 
-    def getHomeEV(self, n):
+    def generateEVData(self, n, mode='home'):
+        # 根据模式选择参数
+        params = self.home_params if mode == 'home' else self.public_params
+
         # 使用正态分布生成充电开始和结束时间
-        t_c = np.random.randn(n) * self.SIGMA_1TC + self.MU_1TC
-        t_c = np.where((self.MU_1TC - 12 < t_c) & (t_c <= 24), t_c, t_c - 24)
+        t_c = np.random.randn(n) * params['SIGMA_TC'] + params['MU_TC']
+        t_c = np.where((params['MU_TC'] - 12 < t_c) & (t_c <= 24), t_c, t_c - 24)
 
-        t_dis = np.random.randn(n) * self.SIGMA_1TDIS + self.MU_1TDIS
-        t_dis = np.where((0 < t_dis) & (t_dis <= self.MU_1TDIS + 12), t_dis, t_dis + 24)
+        t_dis = np.random.randn(n) * params['SIGMA_TDIS'] + params['MU_TDIS']
+        t_dis = np.where((0 < t_dis) & (t_dis <= params['MU_TDIS'] + 12), t_dis, t_dis + 24)
 
         # 转换时间为时隙索引
         J_c = np.ceil(t_c / self.DELTA_T).astype(int)
@@ -38,33 +46,13 @@ class EVChargingDataGenerator:
         SOC_max = np.random.uniform(self.SOC_MAX_A, self.SOC_MAX_B, n)
 
         # 创建包含所有数据的 DataFrame
-        EV_data = pd.DataFrame({'t_c': t_c, 't_dis': t_dis, 'J_c': J_c, 'J_dis': J_dis,
-                                'SOC_con': SOC_con, 'SOC_min': SOC_min, 'SOC_max': SOC_max})
+        EV_data = pd.DataFrame({
+            't_c': t_c, 't_dis': t_dis,
+            'J_c': J_c, 'J_dis': J_dis,
+            'SOC_con': SOC_con, 'SOC_min': SOC_min, 'SOC_max': SOC_max
+        })
         return EV_data
 
-    def getPublicEV(self, n):
-        # 使用正态分布生成充电开始和结束时间
-        t_c = np.random.randn(n) * self.SIGMA_2TC + self.MU_2TC
-        t_c = np.where((0 < t_c) & (t_c <= self.MU_2TC + 12), t_c, t_c - 24)
-
-        t_dis = np.random.randn(n) * self.SIGMA_2TDIS + self.MU_2TDIS
-        t_dis = np.where((self.MU_2TDIS - 12 < t_dis) & (t_dis <= 24), t_dis, t_dis + 24)
-
-        # 转换时间为时隙索引
-        J_c = np.ceil(t_c / self.DELTA_T).astype(int)
-        J_c[J_c == 0] = 96
-        J_dis = np.floor(t_dis / self.DELTA_T).astype(int)
-        J_dis[J_dis == 0] = 96
-
-        # 生成电池状态的随机值
-        SOC_con = np.random.uniform(self.SOC_CON_A, self.SOC_CON_B, n)
-        SOC_min = np.random.uniform(self.SOC_MIN_A, self.SOC_MIN_B, n)
-        SOC_max = np.random.uniform(self.SOC_MAX_A, self.SOC_MAX_B, n)
-
-        # 创建包含所有数据的 DataFrame
-        EV_data = pd.DataFrame({'t_c': t_c, 't_dis': t_dis, 'J_c': J_c, 'J_dis': J_dis,
-                                'SOC_con': SOC_con, 'SOC_min': SOC_min, 'SOC_max': SOC_max})
-        return EV_data
 
 
 class EVChargingOptimizer:
@@ -78,43 +66,28 @@ class EVChargingOptimizer:
         self.DELTA_T = DELTA_T  # 时间间隔
         self.solver = cp.GLPK_MI  # 定义求解器
 
-    def homeChargingPattern(self, EV):
-        # 家庭充电模式下的优化模型
+    def _optimizeChargingPattern(self, EV, mode):
         n = len(EV)
-        P_basic_home = np.roll(self.P_BASIC, -48)  # 将时间调整到以12点为起点
+        P_basic = np.roll(self.P_BASIC, -48) if mode == 'home' else self.P_BASIC.copy()
 
         # 创建二进制优化变量
         x = cp.Variable((n, 96), boolean=True)
 
         # 定义约束和目标函数
-        constraints, objective = self._defineOptimization(EV, x, P_basic_home, mode='home')
+        constraints, objective = self._defineOptimization(EV, x, P_basic)
 
         # 求解优化问题
         problem = cp.Problem(objective, constraints)
         problem.solve(solver=self.solver)
 
         # 返回优化结果
-        return x.value, P_basic_home, (P_basic_home + cp.sum(cp.multiply(x, self.P_SLOW_EV), axis=0)).value
+        return x.value, P_basic, (P_basic + cp.sum(cp.multiply(x, self.P_SLOW_EV), axis=0)).value
 
-    def publicChargingPattern(self, EV):
-        # 公共充电模式下的优化模型
-        n = len(EV)
-        P_basic_public = self.P_BASIC.copy()  # 保持原始负载数据不变
+    def chargingPattern(self, EV, mode='home'):
+        # 根据模式选择家庭或公共充电优化
+        return self._optimizeChargingPattern(EV, mode)
 
-        # 创建二进制优化变量
-        x = cp.Variable((n, 96), boolean=True)
-
-        # 定义约束和目标函数
-        constraints, objective = self._defineOptimization(EV, x, P_basic_public, mode='public')
-
-        # 求解优化问题
-        problem = cp.Problem(objective, constraints)
-        problem.solve(solver=self.solver)
-
-        # 返回优化结果
-        return x.value, P_basic_public, (P_basic_public + cp.sum(cp.multiply(x, self.P_SLOW_EV), axis=0)).value
-
-    def _defineOptimization(self, EV, x, P_basic, mode='home'):
+    def _defineOptimization(self, EV, x, P_basic):
         # 定义通用优化模型
         n = len(EV)
         constraints = []
@@ -188,37 +161,35 @@ class EVDataVisualizer:
         plt.show()
 
 def main():
-    # 实例化类
-    def main():
-        # 实例化数据生成器、优化器和可视化器
-        P_BASIC = pd.read_csv('basicLoadDatacsv.csv', header=None).values.squeeze()
-        print(P_BASIC.shape)
-        data_generator = EVChargingDataGenerator()
-        optimizer = EVChargingOptimizer(P_BASIC)
-        visualizer = EVDataVisualizer()
+    # 实例化数据生成器、优化器和可视化器
+    P_BASIC = pd.read_csv('basicLoadDatacsv.csv', header=None).values.squeeze()
+    data_generator = EVChargingDataGenerator()
+    optimizer = EVChargingOptimizer(P_BASIC)  # 实例化优化器
+    visualizer = EVDataVisualizer()  # 实例化可视化器
 
-        # 生成家庭和公共充电模式下的EV数据
-        EV_home = data_generator.getHomeEV(1000)
-        EV_public = data_generator.getPublicEV(1000)
+    # 生成家庭和公共充电模式下的EV数据
+    EV_home = data_generator.generateEVData(1000, 'home')
+    EV_public = data_generator.generateEVData(1000, 'public')
 
-        # 家庭充电模式下的优化
-        x_home, P_basic_home, P_SOC_crd_home = optimizer.homeChargingPattern(EV_home)
+    # 家庭充电模式下的优化
+    x_home, P_basic_home, P_SOC_crd_home = optimizer.chargingPattern(EV_home, 'home')
 
-        # 公共充电模式下的优化
-        x_public, P_basic_public, P_SOC_crd_public = optimizer.publicChargingPattern(EV_public)
+    # 公共充电模式下的优化
+    x_public, P_basic_public, P_SOC_crd_public = optimizer.chargingPattern(EV_public, 'public')
 
-        # 可视化家庭充电模式下的EV数据
-        visualizer.printEVData(EV_home, "家庭充电模式")
+    # 可视化家庭充电模式下的EV数据
+    visualizer.printEVData(EV_home, "家庭充电模式")
 
-        # 可视化公共充电模式下的EV数据
-        visualizer.printEVData(EV_public, "公共充电模式")
+    # 可视化公共充电模式下的EV数据
+    visualizer.printEVData(EV_public, "公共充电模式")
 
-        # 可视化家庭充电模式的优化结果
-        visualizer.figureResult(P_basic_home, P_SOC_crd_home, "家庭充电模式优化结果")
+    # 可视化家庭充电模式的优化结果
+    visualizer.figureResult(P_basic_home, P_SOC_crd_home, "家庭充电模式优化结果")
 
-        # 可视化公共充电模式的优化结果
-        visualizer.figureResult(P_basic_public, P_SOC_crd_public, "公共充电模式优化结果")
-
+    # 可视化公共充电模式的优化结果
+    visualizer.figureResult(P_basic_public, P_SOC_crd_public, "公共充电模式优化结果")
 
 if __name__ == "__main__":
-    main()
+    main
+
+
